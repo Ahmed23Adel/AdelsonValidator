0146e231de71075a16491266cf84e6fc8fa23745
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2025-07-21 17:21:27 +0200

    adding new validator for checking all are letters

---
+++ Sources/AdelsonValidator/Validators/SingleInputValidators/BasicValidators/StringIsAllLetters.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  AdelsonValidator
+//
+//  Created by ahmed on 21/07/2025.
+//
+
+import Foundation
+public enum StringIsAllLettersError: Error {
+    case containsNonLetterCharacters
+}
+
+public struct StringIsAllLetters: SingleInputValidator {
+    public var input: String
+    public var error: (any Error)?
+
+    public init() {
+        self.input = ""
+    }
+
+    public mutating func setInput(input: String) {
+        self.input = input
+    }
+
+    public mutating func check() -> Bool {
+        let isAllLetters = input.allSatisfy { $0.isLetter }
+        if isAllLetters {
+            return true
+        } else {
+            saveError()
+            return false
+        }
+    }
+
+    public mutating func saveError() {
+        self.error = StringIsAllLettersError.containsNonLetterCharacters
+    }
+}
